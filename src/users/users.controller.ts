9cc3ed55a758ff2924a35c7a7916967461273d6d
pliersch <hourby@gmail.com>
2021-11-26 13:13:22 +0100

    - begin ngxs (auth wip)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,42 +1,37 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
-import { Logger } from '@nestjs/common';
+import {Body, Controller, Get, Logger, Param, Post, Put} from '@nestjs/common';
+import {UsersService} from './users.service';
+import {User} from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
 
-  private readonly logger = new Logger(UsersController.name);
+    private readonly logger = new Logger(UsersController.name);
 
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {
+    }
 
-  @Post()
-  create(@Body() user: User) {
-    return this.usersService.create(user);
-  }
+    @Post()
+    create(@Body() user: User) {
+        return this.usersService.create(user);
+    }
 
-  @Post('login')
-  login(@Body() user: User) {
-    return this.usersService.login(user);
-  }
+    @Post('login')
+    login(@Body() user: User) {
+        return this.usersService.login(user);
+    }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.usersService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(id);
+    }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() user: User) {
-    return this.usersService.update(/*id,*/ user);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.removeOne(id);
-  }
+    @Put(':id')
+    update(@Param('id') id: string, @Body() user: User) {
+        return this.usersService.update(/*id,*/ user);
+    }
 }
