5f2f1019c96d97eab47c01b591122f07c7ada275
pliersch <hourby@gmail.com>
2022-01-07 18:54:01 +0100

    - move to github

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,8 +1,11 @@
-import {Injectable} from '@nestjs/common';
+import {Body, Injectable, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 
-import {Repository} from 'typeorm';
+import {Repository, UpdateResult} from 'typeorm';
 import {Message} from './message.entity';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {Express} from "express";
+import {diskStorage} from "multer";
 
 
 @Injectable()
@@ -26,12 +29,36 @@ export class ChatService {
     return await this.repository.findOne(id);
   }
 
-  async update(message: Message) {
+  async update(message: Message): Promise<UpdateResult> {
     return await this.repository.update(message.id, message);
   }
 
-  async removeOne(id: string) {
+  async removeOne(id: string): Promise<Message> {
     const message = await this.repository.findOne(id);
     return await this.repository.remove(message);
+  }
+
+  // body is type 'any' because we must parse the json string :(
+  @UseInterceptors(FileInterceptor('image', createMulterStorage()))
+  @Post('file')
+  // @ts-ignore
+  uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File): Promise<Message> {
+    // const photo = {} as Photo;
+    // photo.tags = JSON.parse(body.tags);
+    // photo.fileName = 'http://localhost:3000/' + file.filename;
+    // console.log('photo before save in db', photo);
+    // return this.create(photo);
+  }
+}
+
+function createMulterStorage() {
+  return {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: function (req, file, cb) {
+        const extension = file.originalname.substring(file.originalname.lastIndexOf('.'))
+        cb(null, file.fieldname + '-' + Date.now() + extension)
+      }
+    })
   }
 }
--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -4,60 +4,59 @@ import {Photo} from './entites/photo.entity';
 import {FileInterceptor} from '@nestjs/platform-express';
 import {Express} from 'express';
 import {diskStorage} from 'multer';
-import {ConfigService} from "@nestjs/config";
 
 @Controller('photos')
 export class PhotosController {
 
-    constructor(private photoService: PhotosService) {
-    }
+  constructor(private photoService: PhotosService) {
+  }
 
-    @Post()
-    async create(@Body() photo: Photo): Promise<Photo> {
-        console.log('id', photo.id);
-        return this.photoService.create(photo);
-    }
+  @Post()
+  async create(@Body() photo: Photo): Promise<Photo> {
+    console.log('id', photo.id);
+    return this.photoService.create(photo);
+  }
 
-    @Get()
-    async findAll(): Promise<Photo[]> {
-        return this.photoService.findAll();
-    }
+  @Get()
+  async findAll(): Promise<Photo[]> {
+    return this.photoService.findAll();
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: string): Promise<Photo> {
-        return this.photoService.findOne(id);
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Photo> {
+    return this.photoService.findOne(id);
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() photo: Photo) {
-        return this.photoService.update(/*+id, */photo);
-    }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() photo: Photo) {
+    return this.photoService.update(/*+id, */photo);
+  }
 
-    @Delete(':id')
-    removeOne(@Param('id') id: string) {
-        return this.photoService.removeOne(id);
-    }
+  @Delete(':id')
+  removeOne(@Param('id') id: string) {
+    return this.photoService.removeOne(id);
+  }
 
-    // body is type 'any' because we must parse the json string :(
-    @UseInterceptors(FileInterceptor('image', createMulterStorage()))
-    @Post('file')
-    uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
-        const photo = {} as Photo;
-        photo.tags = JSON.parse(body.tags);
-        photo.fileName = 'http://localhost:3000/' + file.filename;
-        console.log('photo before save in db', photo);
-        return this.create(photo);
-    }
+  // body is type 'any' because we must parse the json string :(
+  @UseInterceptors(FileInterceptor('image', createMulterStorage()))
+  @Post('file')
+  uploadFile(@Body() body: any, @UploadedFile() file: Express.Multer.File) {
+    const photo = {} as Photo;
+    photo.tags = JSON.parse(body.tags);
+    photo.fileName = 'http://localhost:3000/' + file.filename;
+    console.log('photo before save in db', photo);
+    return this.create(photo);
+  }
 }
 
 function createMulterStorage() {
-    return {
-        storage: diskStorage({
-            destination: './uploads',
-            filename: function (req, file, cb) {
-                const extension = file.originalname.substring(file.originalname.lastIndexOf('.'))
-                cb(null, file.fieldname + '-' + Date.now() + extension)
-            }
-        })
-    }
+  return {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: function (req, file, cb) {
+        const extension = file.originalname.substring(file.originalname.lastIndexOf('.'))
+        cb(null, file.fieldname + '-' + Date.now() + extension)
+      }
+    })
+  }
 }
