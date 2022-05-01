6e733e2484431e02527d3784abac90e72a78d6cc
pliersch <hourby@gmail.com>
2022-05-01 10:46:33 +0200

    - download of selected pictures success

--- src/download/download.controller.ts
+++ src/download/download.controller.ts
@@ -1,7 +1,7 @@
-import {Controller, Get, Response, StreamableFile} from '@nestjs/common';
+import {Body, Controller, Get, Post, Response, StreamableFile} from '@nestjs/common';
 import {DownloadService} from "./download.service";
-import {createReadStream} from "fs";
-import {join} from "path";
+import {createReadStream, createWriteStream, writeFile} from "fs";
+import {join, resolve} from "path";
 import * as JSZip from 'jszip';
 
 @Controller('download')
@@ -10,30 +10,26 @@ export class DownloadController {
   constructor(private readonly service: DownloadService) {
   }
 
-  @Get()
-  async getFile(@Response({passthrough: true}) res): Promise<StreamableFile> {
+  @Post()
+  async getFile(@Body() fileNames: string[], @Response({passthrough: true}) res): Promise<StreamableFile> {
+    const result = 'pictures_' + new Date().getTime() + '.zip';
     const zip = new JSZip();
-    zip.file("hello.txt", "Hello World\n");
-    if (JSZip.support.uint8array) {
-      await zip.generateAsync({type: "uint8array"});
-    } else {
-      await zip.generateAsync({type: "string"});
+    const img = zip.folder("pictures");
+    const pictureFolder: string = resolve('./static/images/gallery/full');
+    let picture, file;
+    for (const fileName of fileNames) {
+      file = fileName + '.jpg';
+      picture = createReadStream(join(pictureFolder, file));
+      img.file(file, picture, {base64: true});
     }
 
-    // zip
-    //   .generateNodeStream({type: 'nodebuffer', streamFiles: true})
-    //   .pipe(createWriteStream(join(process.cwd(), 'foo.zip')))
-    //   .on('finish', function () {
-    //     console.log("out.zip written.");
-    //   });
+    const uint8Array = await zip.generateAsync({type: "uint8array"});
 
-    const file = createReadStream(join(process.cwd(), 'out.zip'));
     res.set({
       'Content-Type': 'application/zip',
-      'Content-Disposition': 'attachment; filename="package.zip"',
+      'Content-Disposition': 'attachment; filename=' + result,
     });
-    console.log('DownloadController getFile: ',)
-    return new StreamableFile(file)
+    return new StreamableFile(uint8Array)
   }
 
 }
