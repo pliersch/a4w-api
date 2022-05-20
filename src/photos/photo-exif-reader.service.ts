47d5e77b4878874f2ac76901e63f3212f9beacb3
pliersch <hourby@gmail.com>
2022-05-20 08:46:32 +0200

    - add exif reader (current not used)

---
+++ src/photos/photo-exif-reader.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import * as ExifReader from 'exifreader';
+import * as path from "path";
+import * as fs from "fs";
+
+@Injectable()
+export class PhotoExifReaderService {
+
+
+  constructor() {
+    // this.readDirectory('./static/images/gallery/full');
+  }
+
+  async readDirectory(inputPath: string): Promise<void> {
+    console.log('PhotoExifReaderService readDirectory: ', inputPath)
+    const inputDirectory = path.resolve(inputPath)
+    console.log('PhotoExifReaderService readDirectory: ', inputDirectory)
+    const files = await fs.promises.readdir(inputDirectory);
+    for (const file of files) {
+      console.log('PhotoExifReaderService readDirectory: ', file)
+      await this.read(inputDirectory + '\\' + file)
+    }
+  }
+
+  async read(file: string): Promise<void> {
+    console.log('PhotoExifReaderService read: ', file)
+    const tags = await ExifReader.load(file);
+    const imageDate = tags['DateTimeOriginal'].description;
+    const unprocessedTagValue = tags['DateTimeOriginal'].value;
+    console.log('PhotoExifReaderService read: ', imageDate, unprocessedTagValue)
+  }
+}
--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,18 +1,20 @@
-import {Module} from '@nestjs/common';
-import {PhotosService} from './photos.service';
-import {PhotosController} from './photos.controller';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {Photo} from './entites/photo.entity';
-import {ConfigModule} from "@nestjs/config";
-import {PhotoProcessorService} from './photo-processor.service';
-import {SharpService} from "nestjs-sharp";
+import { Module } from '@nestjs/common';
+import { PhotosService } from './photos.service';
+import { PhotosController } from './photos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Photo } from './entites/photo.entity';
+import { ConfigModule } from "@nestjs/config";
+import { PhotoProcessorService } from './photo-processor.service';
+import { SharpService } from "nestjs-sharp";
+
+// import { PhotoExifReaderService } from "./photo-exif-reader.service";
 
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([Photo])
   ],
-  providers: [PhotosService, PhotoProcessorService, SharpService],
+  providers: [PhotosService, PhotoProcessorService, /*PhotoExifReaderService,*/ SharpService],
   controllers: [PhotosController]
 })
 export class PhotosModule {
