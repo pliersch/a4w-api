bc4c539e54237d8de21c0cc39d38bcca35edfd8c
pliersch <hourby@gmail.com>
2022-09-21 17:41:59 +0200

    - fix file extension

--- src/modules/download/download.controller.ts
+++ src/modules/download/download.controller.ts
@@ -1,7 +1,7 @@
-import {Body, Controller, Get, Post, Response, StreamableFile} from '@nestjs/common';
-import {DownloadService} from "./download.service";
-import {createReadStream, createWriteStream, writeFile} from "fs";
-import {join, resolve} from "path";
+import { Body, Controller, Post, Response, StreamableFile } from '@nestjs/common';
+import { DownloadService } from "./download.service";
+import { createReadStream } from "fs";
+import { join, resolve } from "path";
 import * as JSZip from 'jszip';
 
 @Controller('download')
@@ -16,11 +16,10 @@ export class DownloadController {
     const zip = new JSZip();
     const img = zip.folder("pictures");
     const pictureFolder: string = resolve('./static/images/gallery/full');
-    let picture, file;
+    let picture;
     for (const fileName of fileNames) {
-      file = fileName + '.jpg';
-      picture = createReadStream(join(pictureFolder, file));
-      img.file(file, picture, {base64: true});
+      picture = createReadStream(join(pictureFolder, fileName));
+      img.file(fileName, picture, {base64: true});
     }
 
     const uint8Array = await zip.generateAsync({type: "uint8array"});
