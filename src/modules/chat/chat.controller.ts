43ab762c5b767daa0979072ee072b0c869f644b2
pliersch <hourby@gmail.com>
2023-02-13 07:24:47 +0100

    - wip 'chat'

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,6 +1,7 @@
 import { CreateMessageDto } from "@modules/chat/create-message-dto";
 import { User } from "@modules/users/entities/user.entity";
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Sse } from '@nestjs/common';
+import { Observable, Subject } from "rxjs";
 import { getPostgresDataSource } from "../../postgres.datasource";
 import { ChatService } from './chat.service';
 import { Message } from './message.entity';
@@ -10,17 +11,29 @@ export class ChatController {
 
   constructor(private readonly service: ChatService) { }
 
+  // server sent MUST BE UNDER CONSTRUCTOR. OTHERWISE, A TYPEORM ERROR WILL THROW
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return this.changes$.asObservable();
+  }
+
+  private changes$: Subject<MessageEvent> = new Subject()
+
+  private sendEvent(event: MessageEvent) {
+    console.log('ChatController sendEvent: ', event)
+    this.changes$.next(event)
+  }
+
   @Post()
   async create(@Body() dto: CreateMessageDto) {
-    console.log('ChatController create: ', dto)
     const dataSource = await getPostgresDataSource();
     const userRepository = await dataSource.manager.getRepository(User);
     const user = await userRepository.findOneBy({id: dto.userId});
-    console.log('ChatController create: ', user)
     const msg: Message = new Message();
     msg.text = dto.text;
     msg.user = user;
     msg.fileNames = null;
+    setTimeout(() => this.sendEvent({data: {type: 'message_added'}} as MessageEvent), 500);
     return this.service.create(msg);
   }
 
