88f9cae6eefabfe9a2a21c99868e183c1ceb95f9
Patrick Liersch <hourby@gmail.com>
2023-07-13 17:43:29 +0200

    - working on hosts and ports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as fs from "fs";
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/apps4web.de/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/apps4web.de/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/apps4web.de/chain.pem', 'utf8');
+
+  const httpsOptions = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions,
     logger: console
   });
 
@@ -20,7 +33,7 @@ async function bootstrap() {
   let port = 3000;
 
   if (process.env.NODE_ENV === 'production') {
-    origin = 'http://127.0.0.1:4300';
+    origin = 'https://apps4web.de';
     port = 3300;
   }
 
@@ -30,7 +43,7 @@ async function bootstrap() {
 
   app.enableCors({credentials: true, origin: origin});
   // app.useWebSocketAdapter(new A4WAdapter(app));
-  await app.listen(port, '0.0.0.0');
+  await app.listen(3300, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
